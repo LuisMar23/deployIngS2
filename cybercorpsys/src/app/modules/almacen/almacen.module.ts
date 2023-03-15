4f6aea3626b8780f66e22666ca010659a522f722
skeytor <srleon069@gmail.com>
2023-03-14 21:55:17 -0400

    US14 - REFACTORIZAR - ROBER

--- cybercorpsys/src/app/modules/almacen/almacen.module.ts
+++ cybercorpsys/src/app/modules/almacen/almacen.module.ts
@@ -5,11 +5,13 @@ import { AlmacenRoutingModule } from './almacen-routing.module';
 import { EgresoComponent } from './egreso/egreso.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ReactiveFormsModule } from '@angular/forms';
+import { DialogEgresoComponent } from './egreso/dialog-egreso/dialog-egreso.component';
 
 
 @NgModule({
   declarations: [
-    EgresoComponent
+    EgresoComponent,
+    DialogEgresoComponent
   ],
   imports: [
     CommonModule,
---
+++ cybercorpsys/src/app/modules/almacen/egreso/dialog-egreso/dialog-egreso.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dialog-egreso',
+  templateUrl: './dialog-egreso.component.html',
+  styleUrls: ['./dialog-egreso.component.css']
+})
+export class DialogEgresoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
